c5a3842d5777ed4bf3f971f914672da934665565
Andrei Poliakov <avpgenium@yandex.ru>
2021-03-11 14:37:36 +0300

    HomeWork 2: - add components Header, Layout and Footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <App>
+      <Header id={1} title={"This is title"} desc={"This is Description!"}/>
+      <Layout id={2} title={"This is title"} desc={"This is Description!"} urlBg={"./bg1.jpg"}/>
+      <Layout id={3} title={"This is title"} desc={"This is Description!"} colorBg={"red"}/>
+      <Layout id={4} title={"This is title"} desc={"This is Description!"} urlBg={"./bg3.jpg"}/>
+      <Footer />
+    </App>
   );
 }
 
---
+++ src/components/Layout/index.js
@@ -0,0 +1,23 @@
+import s from './style.module.css';
+
+function Layout({id, title, desc, urlBg, colorBg}) {
+    const styleBg = urlBg ? {backgroundImage: `url(${urlBg})`} : {background: `${colorBg}`}
+
+    return (
+        <section className="root" id={id} style={styleBg}>
+            <div className={s.wrapper}>
+                <article>
+                    <div className={s.title}>
+                        <h3>${title}</h3>
+                        <span className={s.separator}></span>
+                    </div>
+                    <div className={`${s.desc} ${s.full}`}>
+                        <p>${desc}</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    );
+}
+
+export default Layout;
