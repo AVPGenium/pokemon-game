03f529d83dd9e72a62a142ec5b6a563c6d67f245
Andrei Poliakov <avpgenium@yandex.ru>
2021-03-12 09:39:16 +0300

    HomeWork 2: - fixes for merge request

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import Header from "./components/Header";
 import Layout from "./components/Layout";
 import Footer from "./components/Footer";
 
+import bg1 from "./bg1.jpg"
+import bg3 from "./bg3.jpg"
+
 function App() {
   return (
     <>
-      <Header id={1} title={"This is title"} desc={"This is Description!"}/>
-      <Layout id={2} title={"This is title"} desc={"This is Description!"} urlBg={"./bg1.jpg"} />
-      <Layout id={3} title={"This is title"} desc={"This is Description!"} colorBg={"red"} />
-      <Layout id={4} title={"This is title"} desc={"This is Description!"} urlBg={"./bg3.jpg"} />
+      <Header title="This is title" desc="This is Description!"/>
+      <Layout id={2} title="This is title" desc="This is Description!" urlBg={bg1} />
+      <Layout id={3} title="This is title" desc="This is Description!" colorBg="red" />
+      <Layout id={4} title="This is title" desc="This is Description!" urlBg={bg3} />
       <Footer />
     </>
   );
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,18 +1,25 @@
-import s from './style.module.css';
+import styles from './style.module.css';
+import s from "../Header/style.module.css";
 
 function Layout({id, title, desc, urlBg, colorBg}) {
-    const styleBg = urlBg ? {backgroundImage: `url(${urlBg})`} : {background: `${colorBg}`}
-
+    //const styleBg = urlBg ? {backgroundImage: `url(${urlBg})`} : {background: `${colorBg}`}
+    const  s = {};
+    if (urlBg) {
+        s.backgroundImage = `url(${urlBg})`;
+    }
+    if (colorBg) {
+        s.backgroundColor = colorBg
+    }
     return (
-        <section className="root" id={id} style={styleBg}>
-            <div className={s.wrapper} style={styleBg}>
+        <section className={styles.root} id={id} style={s}>
+            <div className={styles.wrapper}>
                 <article>
-                    <div className={s.title}>
-                        <h3>${title}</h3>
-                        <span className={s.separator}></span>
+                    <div className={styles.title}>
+                        <h3>{title}</h3>
+                        <span className={styles.separator}></span>
                     </div>
-                    <div className={`${s.desc} ${s.full}`}>
-                        <p>${desc}</p>
+                    <div className={`${styles.desc} ${styles.full}`}>
+                        <p>{desc}</p>
                     </div>
                 </article>
             </div>
