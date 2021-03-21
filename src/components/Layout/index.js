278ae3546ad67baf1ac10e25bb598cc3e6d5448d
Andrei Poliakov <avpgenium@yandex.ru>
2021-03-21 17:53:07 +0300

    HomeWork 6: + use firebase to persist data + "add pokemon" button

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,7 +1,6 @@
 import styles from './style.module.css';
 
 function Layout({id, title, desc, urlBg, colorBg, children}) {
-    //const styleBg = urlBg ? {backgroundImage: `url(${urlBg})`} : {background: `${colorBg}`}
     const  s = {};
     if (urlBg) {
         s.backgroundImage = `url(${urlBg})`;
@@ -15,7 +14,7 @@ function Layout({id, title, desc, urlBg, colorBg, children}) {
                 <article>
                     <div className={styles.title}>
                         <h3>{title}</h3>
-                        <span className={styles.separator}></span>
+                        <span className={styles.separator}/>
                     </div>
                     <div className={`${styles.desc} ${styles.full}`}>
                         <p>{children}</p>
--- src/routes/Game/index.js
+++ src/routes/Game/index.js
@@ -1,38 +1,108 @@
 import Layout from "../../components/Layout";
-import bg3 from "../Home/bg3.jpg";
-import s from "../Home/style.module.css";
-import pokemonsData from "../../pokemonsData.json";
+import bg3 from "./bg3.jpg";
+import s from "./style.module.css";
 import PokemonCard from "../../components/PokemonCard";
-import {useHistory} from "react-router-dom";
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import database from '../../service/firebase';
 
+// TODO: add form for adding new pokemon and create class for storing object entity model
+const createTemplatePokemon = (id) => ({
+    "abilities" : [ "keen-eye", "tangled-feet", "big-pecks" ],
+    "base_experience" : 122,
+    "height" : 11,
+    id,
+    "img" : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png`,
+    "name" : "pidgeotto",
+    "stats" : {
+        "attack" : 60,
+        "defense" : 55,
+        "hp" : 63,
+        "special-attack" : 50,
+        "special-defense" : 50,
+        "speed" : 71
+    },
+    "type" : "flying",
+    "values" : {
+        "bottom" : 7,
+        "left" : 5,
+        "right" : 2,
+        "top" : "A"
+    }
+});
 
 const GamePage = () => {
+    const [pokemons, setPokemons] = useState({});
 
-    const [pokemons, setPokemons] = useState(pokemonsData);
+    useEffect(() => {
+        database.ref('pokemons').once('value', (snapshot) => {
+            setPokemons(snapshot.val())
+        })
+    }, []);
 
     const onCardClick = (id) => {
-        setPokemons((pokemons) => {
-            return pokemons.map(item => item.id === id ? ({...item, active: !item.active}) : item)
-        })
+        let objID = null;
+
+        const updatedPokemons = Object.entries(pokemons).reduce((acc, item) => {
+            const pokemon = {...item[1]};
+
+            if (pokemon.id === id) {
+                objID =	 item[0];
+                pokemon.active = !pokemon.active;
+            };
+
+            acc[item[0]] = pokemon;
+            return acc;
+        }, {});
+
+        if (objID) {
+            const updatedPokemon = updatedPokemons[objID];
+
+            database.ref('pokemons/' + objID).set(updatedPokemon)
+                .then(() => setPokemons(updatedPokemons));
+        }
+    }
+
+    const addNewPokemon = () => {
+        const allIds = Object.values(pokemons).map(pok => pok.id);
+        const setIds = new Set(allIds);
+        let id = 0;
+        let exists = setIds.has(id);
+
+        while (exists) {
+            id += 1;
+            exists = setIds.has(id);
+        }
+
+        const data = createTemplatePokemon(id);
+
+        const newKey = database.ref().child('pokemons').push().key;
+        database.ref('pokemons/' + newKey).set(data)
+            .then(() => {
+                const updatedPokemons = {...pokemons, [newKey]: data};
+                setPokemons(updatedPokemons);
+            });
     }
 
     return (
         <>
             <Layout id={4} title="Battlefield" desc="This is Description!" urlBg={bg3} >
+                <div className={s.btn_wrap}>
+                    <button className={s.add_btn}
+                            onClick={addNewPokemon}>
+                        Add pokemon
+                    </button>
+                </div>
+
                 <div className={s.flex}>
-                    {
-                        pokemonsData.map(item =>
-                            <PokemonCard
-                                key={item.id}
-                                id={item.id}
-                                name={item.name}
-                                img={item.img}
-                                type={item.type}
-                                values={item.values}
-                                onCardClick={onCardClick}
-                                isActive={item.isActive}
-                            />)
+                    { Object.entries(pokemons).map(([key, value]) =>
+                        <PokemonCard key={key}
+                                     name={value.name}
+                                     img={value.img}
+                                     id={value.id}
+                                     type={value.type}
+                                     values={value.values}
+                                     isActiveCard={value.active}
+                                     onCardClick={onCardClick}/>)
                     }
                 </div>
             </Layout>
