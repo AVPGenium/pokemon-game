b5012dfd764f8ed844e777fdee588eda99964507
Andrei Poliakov <avpgenium@yandex.ru>
2021-03-28 20:14:10 +0300

    HomeWork 9: use redux

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import {BrowserRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store/store';
 
 import './index.css';
 import App from './App';
 
 const MainApp = () => {
     return (
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
     )
 }
 
